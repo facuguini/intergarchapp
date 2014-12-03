e5455eba32535a137a9b1f7953ffe54e6de659f1
unknown <fguini@DMIPC81.rojas.loc>
2014-12-03 12:06:51 -0300

    estadisticasgit add .! nuevo jug, manejo de excepciones

--- js/controllers/estadisticasController.js
+++ js/controllers/estadisticasController.js
@@ -1,29 +1,34 @@
 app.controller('estadisticasController', ['$scope', '$timeout', function($scope, $timeout) {
 	
 	$scope.equipos = [];
-	$("#itable").load(function(){
-		$(this).contents().each($('.posiciones').children(".items").children().children(".col1").children(), function(i){
-			var equipo = {
-				nombre: $(this).text(),
-				pj: "",
-				dg: "",
-				ptos: "" //html()
-			}
-			$scope.equipos[i] = equipo;
+
+	try {
+    	$("#itable").load(function(){
+			$.each($(this).contents().find('.posiciones').children(".items").children().children(".col1").children(), function(i){
+				var equipo = {
+					nombre: $(this).text(),
+					pj: "",
+					dg: "",
+					ptos: "" //html()
+				}
+				$scope.equipos[i] = equipo;
+			})
+			$.each($(this).contents().find('.posiciones').children(".items").children().children(".col2"), function(i){
+				$scope.equipos[i].pj = $(this).html()
+			})
+			$.each($(this).contents().find('.posiciones').children(".items").children().children(".col8"), function(i){
+				$scope.equipos[i].dg = $(this).html()
+			})
+			$.each($(this).contents().find('.posiciones').children(".items").children().children(".col10"), function(i){
+				$scope.equipos[i].ptos = $(this).html()
+			})
 		})
-		$(this).contents().each($('.posiciones').children(".items").children().children(".col2"), function(i){
-			$scope.equipos[i].equipo.pj = $(this).text()
-		})
-		$(this).contents().each($('.posiciones').children(".items").children().children(".col8"), function(i){
-			$scope.equipos[i].equipo.dg = $(this).text()
-		})
-		$(this).contents().each($('.posiciones').children(".items").children().children(".col10"), function(i){
-			$scope.equipos[i].equipo.ptos = $(this).text()
-		})
-	})
-/*	$.each($('.posiciones').children(".items").children().children(".col1").children(), function() { 
-    	console.log($(this).text());
-	});*/
+    	localStorage.setItem("estadistica", $scope.equipos)
+	} catch(err) {
+    	$scope.$parent.ErrorDeConexion = "Error de Conexion, no actualizado."
+    	console.log("Error: " + err)
+    	$scope.equipos = localStorage.getItem("estadistica")
+	}
 
 	$(".ui-table-columntoggle-btn").hide();
 	$scope.iframeshow = false;
--- js/controllers/jugadoresController.js
+++ js/controllers/jugadoresController.js
@@ -1,12 +1,11 @@
 app.controller('jugadoresController', ['$scope', '$http','$timeout', function($scope, $http, $timeout) {
 	$scope.jugadores = []
-	$scope.error = ""
+	$scope.nombreNuevo = ""
 
 	var get = function(){
 		$http.get("http://itshare.ddns.net:9290/getjugadores").
 		success(function(data, status){
 			$scope.jugadores = data
-			//$scope.error = status
 			localStorage.setItem("listajugadores", JSON.stringify($scope.jugadores))
 			contador()
 			$scope.$parent.ErrorDeConexion = null
@@ -14,30 +13,45 @@ app.controller('jugadoresController', ['$scope', '$http','$timeout', function($s
 		error(function(status){
 			$scope.jugadores = JSON.parse(localStorage.getItem("listajugadores"))
 			$scope.$parent.ErrorDeConexion = "Error de Conexion, no actualizado."
+			console.log("Error al traer los jugadores: "+data+". Error codigo: "+status)
 		})
 	}
 	get()
 	$scope.check = function(id, check) {
 		console.log("http://itshare.ddns.net:9290/check/"+id+"/"+!check)
 		$http.post("http://itshare.ddns.net:9290/check/"+id+"/"+!check, "").
-		success(function(data, status){
-			console.log(data+" , "+status)
+		success(function(){
 			get()
 		}).
-		error(function() {
-			console.log(2)
+		error(function(data, status) {
+			console.log("Error al chequear: "+data+". Error codigo: "+status)
 		});
 	}
 
 	$scope.delete = function(id) {
 		$http.delete("http://itshare.ddns.net:9290/user/"+id).
 		success(function(data, status){
-			console.log(id+" eliminado." + "data: "+data + ", status: "+status)
+			console.log("Data: "+data + ". Coidgo: "+status)
 			get()
 		}).
-		error(function() {
-			console.log("data: "+data + ", status: "+status)
+		error(function(data, status) {
+			console.log("Error al eliminar: "+data+". Error codigo: "+status)
 		})
+	}
+
+	$scope.nuevo = function() {
+		if(!isNullOrWhiteSpace($scope.nombreNuevo) && ($scope.nombreNuevo.length<30) && ($scope.nombreNuevo.length>2)) {
+			$http.post("http://itshare.ddns.net:9290/user/"+$scope.nombreNuevo, "POST").
+			success(function(){
+				get()
+			}).
+			error(function(data, status) {
+				console.log("Error al crear: "+data+". Error codigo: "+status)
+			})
+			$scope.nombreNuevo = ""
+		} else {
+			alert("nop!")
+		}
 	}
 
 	$scope.$parent.refresh = function() {
@@ -51,5 +65,10 @@ app.controller('jugadoresController', ['$scope', '$http','$timeout', function($s
 				$scope.$parent.cantJugadores++;
 			}
 		}
+	}
+
+	function isNullOrWhiteSpace( input ) {
+	    if (input == null) return true;
+	    return input.replace(/\s/g, '').length < 1;
 	}
 }]);
